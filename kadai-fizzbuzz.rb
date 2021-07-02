ce61068112956317ba39c3b62a82edd3e12ece8c
yuya.hamasaki <aruma_2016@yahoo.co.jp>
2021-07-02 09:16:13 +0000

    third commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(num)
   if num % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
-  else num
+    "Buzz"
+  else
+    num
   end
 end
 
